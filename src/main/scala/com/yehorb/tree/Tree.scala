bdec64980bdcb902712f8fb8e5ee3089ad23aacc
yehorb <egor.borkov.1998@gmail.com>
2018-04-03 11:56:44 +0300

    Extended Node functionality and test coverage

--- src/main/scala/com/yehorb/tree/Tree.scala
+++ src/main/scala/com/yehorb/tree/Tree.scala
@@ -21,7 +21,61 @@ case class Leaf[A](value: A) extends Tree[A]
   * @param children Children list of the node.
   * @tparam A       Type parameter of the node.
   */
-case class Node[A](children: List[Tree[A]]) extends Tree[A]
+case class Node[A](children: List[Tree[A]]) extends Tree[A] {
+  /**
+    * Splits children to nodes and leafs and sorts leafs of given node using provided comparator function
+    * @param sort Comparator function for comparing arguments of type A
+    * @return     New node with nodes and leafs separated, leafs sorted
+    */
+  def normalize(sort: (A, A) => Boolean): Node[A] = {
+    val (nodes, leafs) = _splitSort(sort)
+    this.copy(nodes ++ leafs)
+  }
+
+  // Private node logic for manipulating children list
+  private def _split: (List[Node[A]], List[Leaf[A]]) = {
+    def loop(list: List[Tree[A]], nodes: List[Node[A]], leafs: List[Leaf[A]]): (List[Node[A]], List[Leaf[A]]) = {
+      list match {
+        case s :: xs => s match {
+          case n: Node[A] => loop(xs, n :: nodes, leafs)
+          case l: Leaf[A] => loop(xs, nodes, l :: leafs)
+        }
+        case Nil => (nodes, leafs)
+      }
+    }
+
+    loop(children, Nil, Nil)
+  }
+
+  private def _sort(list: List[Leaf[A]], how: (A, A) => Boolean): List[Leaf[A]] = {
+    def merge(left: List[Leaf[A]], right: List[Leaf[A]]): List[Leaf[A]] =
+      (left, right) match {
+        case (_, Nil) => left
+        case (Nil, _) => right
+        case (leftHead :: leftTail, rightHead :: rightTail) =>
+          if (how(leftHead.value, rightHead.value))
+            leftHead :: merge(leftTail, right)
+          else
+            rightHead :: merge(left, rightTail)
+      }
+
+    def sort(list: List[Leaf[A]]): List[Leaf[A]] = {
+      list.length / 2 match {
+        case 0 => list
+        case n =>
+          val (left, right) = list.splitAt(n)
+          merge(sort(left), sort(right))
+      }
+    }
+
+    sort(list)
+  }
+
+  private def _splitSort(how: (A, A) => Boolean): (List[Node[A]], List[Leaf[A]]) = {
+    val (nodes, leafs) = _split
+    (nodes, _sort(leafs, how))
+  }
+}
 object Node {
   /**
     * Construct new node from List[ Tree[A] ] or the varargs of type Tree[A]
--- src/test/scala/com/yehorb/tree/TreeSpec.scala
+++ src/test/scala/com/yehorb/tree/TreeSpec.scala
@@ -2,12 +2,51 @@ package com.yehorb.tree
 
 import org.scalatest.{FlatSpec, Matchers}
 
-class TreeSpec extends FlatSpec with Matchers {
+trait IntStuff {
+  implicit val zero: Int = 0
+
+  val lessThan: (Int, Int) => Boolean = (a, b) => a < b
+  val moreThan: (Int, Int) => Boolean = (a, b) => a > b
+
+  val lessEqualsThan: (Int, Int) => Boolean = (a, b) => a <= b
+  val moreEqualsThan: (Int, Int) => Boolean = (a, b) => a >= b
+
+  val add: (Int, Int) => Int = (a, b) => a + b
+}
+
+trait FunStuff {
+  val alwaysTrue: (Nothing, Nothing) => Boolean = (_, _) => true
+  val alwaysFalse: (Nothing, Nothing) => Boolean = (_, _) => false
+}
+
+class TreeSpec extends FlatSpec with Matchers with IntStuff with FunStuff {
   "Node companion object" should "properly make new tree from individual pieces" in {
     Node(Leaf(1), Leaf(2), Node(Leaf(3), Leaf(4)), Leaf(5)) shouldEqual new Node(List(Leaf(1), Leaf(2), Node(List(Leaf(3), Leaf(4))), Leaf(5)))
   }
 
   "Node companion object" should "properly make new tree from List" in {
     Node(List(Leaf(1), Leaf(2), Node(List(Leaf(3), Leaf(4))), Leaf(5))) shouldEqual new Node(List(Leaf(1), Leaf(2), Node(List(Leaf(3), Leaf(4))), Leaf(5)))
+  }
+
+  "Node" should "rearrange children so nodes first, leafs last and sort leafs" in {
+    val testNode = Node(Leaf(5), Leaf(3), Node(Leaf(4), Leaf(1)), Leaf(6), Node[Int](Nil))
+    testNode.normalize(lessThan) shouldEqual Node(Node[Int](Nil), Node(Leaf(4), Leaf(1)), Leaf(3), Leaf(5), Leaf(6))
+    testNode.normalize(moreThan) shouldEqual Node(Node[Int](Nil), Node(Leaf(4), Leaf(1)), Leaf(6), Leaf(5), Leaf(3))
+  }
+
+  "Empty node" should "not change" in {
+    val testNode = Node(Nil)
+    testNode.normalize(alwaysTrue) shouldEqual Node(Nil)
+  }
+
+  "Node with leafs only" should "sort leafs" in {
+    val testNode = Node(Leaf(2), Leaf(1), Leaf(3))
+    testNode.normalize(lessThan) shouldEqual Node(Leaf(1), Leaf(2), Leaf(3))
+    testNode.normalize(moreThan) shouldEqual Node(Leaf(3), Leaf(2), Leaf(1))
+  }
+
+  "Nde with nodes only" should "not change" in {
+    val testNode = Node(Node(Nil), Node(Nil), Node(Nil))
+    testNode.normalize(alwaysTrue) shouldEqual Node(Node(Nil), Node(Nil), Node(Nil))
   }
 }
